ec9ad8c6162f770620daa3a7ac05726819af5152
Willian Reis <willian.fagoc@gmail.com>
2020-04-25 19:11:15 -0300

    Adiciona Listar de categoriasna API

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -1,4 +1,5 @@
 const BaseRoute = require('./base/baseRoute')
+const Joi = require('@hapi/joi');
 
 class HeroRoutes extends BaseRoute {
     constructor(db) {
@@ -10,8 +11,32 @@ class HeroRoutes extends BaseRoute {
         return {
             path: '/categories',
             method: 'GET',
+            options: {
+                validate: {
+                    failAction: (request, headers, error) => {
+                        throw error;
+                    },
+                    query: Joi.object({
+                       s: Joi.number().integer().default(0),
+                       l: Joi.number().integer().default(10),
+                       d: Joi.string().min(3).max(100).default('')
+                    })
+                }
+            },
             handler: (request, head) => {
-                return this.contextDb.read()
+                try {
+                    const {s, l, d} = request.query
+
+                    let query = d ? {
+                        description: {$regex: `.*${d}*.`}
+                    } : {}
+
+                    return this.contextDb.read(query, parseInt(s), parseInt(l))
+                } catch (error) {
+                    console.log(error)
+                    return 'Erro no servidor'
+                }
+                
             }
         }
     }
--- src/tests/apiCategories.test.js
+++ src/tests/apiCategories.test.js
@@ -2,7 +2,7 @@ const assert = require('assert')
 const api = require('./../api')
 let app = {}
 
-describe('Category API testing suite', function() {
+describe.only('Category API testing suite', function() {
     
     this.beforeAll(async () => {
         app = await api
@@ -20,5 +20,44 @@ describe('Category API testing suite', function() {
 
         assert.deepEqual(statusCode, 200)
         assert.ok(Array.isArray(data))
+    })
+
+    it('List Category with limit /categories?l=5', async () => {
+        const LIMIT = 5
+        const result = await app.inject({
+            method: 'GET',
+            url: `/categories?l=${LIMIT}`
+        })
+
+        const data = JSON.parse(result.payload)
+        const statusCode = result.statusCode
+
+        assert.deepEqual(statusCode, 200)
+        assert.ok(Array.isArray(data))
+        assert.ok(data.length <= LIMIT)
+    })
+
+    it('List Category with limit /categories?l=AAAE', async () => {
+        const LIMIT = 'WIL'
+        const result = await app.inject({
+            method: 'GET',
+            url: `/categories?l=${LIMIT}`
+        })
+
+        const statusCode = result.statusCode
+        const errorResult = {
+          "statusCode": 400,
+          "error": "Bad Request",
+          "message": "\"l\" must be a number",
+          validation: {
+            keys: [
+              'l'
+            ],
+            source: 'query'
+          }
+        }
+
+        assert.deepEqual(statusCode, 400)
+        assert.deepEqual(errorResult, JSON.parse(result.payload))
     })
 })
